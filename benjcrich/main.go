86d34fcd74ca3414749a769e74401d74dfe6e262
Benj Rich <benjrich@divvy.co>
2022-08-10 15:33:53 -0600

    final

--- benjcrich/main.go
+++ benjcrich/main.go
@@ -24,16 +24,16 @@ type StarshipResponse struct {
 
 func main() {
 
-	const s = `   .           .        .                     .          .            .
-    .               .    .          .              .   .         .
-          _________________      ____         __________
-    .    /                 |    /    \    .  |          \
-.       /     _____   _____| . /      \      |    ___    |     .     .
-        \    \    |   |       /   /\   \     |   |___>   |
-      .  \    \   |   |      /   /__\   \  . |         _/               .
-.    _____>    |  |   | .   /            \   |   |\    \_______    .
-    |          /  |   |    /    ______    \  |   | \           |
-    |_________/   |___|   /____/      \____\ |___|  \__________|    .
+	const s = `   .           .        .                .               .            .
+    .               .    .          .           .      .          .
+            _________________      ____         __________
+    .      /                 |    /    \    .  |          \
+.         /     _____   _____| . /      \      |    ___    |     .     .
+          \    \    |   |       /   /\   \     |   |___>   |
+      .    \    \   |   |      /   /__\   \  . |         _/               .
+.    _______>    |  |   | .   /            \   |   |\    \________    .
+    |            /  |   |    /    ______    \  |   | \            |
+    |___________/   |___|   /____/      \____\ |___|  \___________|    .
 .     ____    __  . _____   ____      .  __________   .  _________
      \    \  /  \  /    /  /    \       |          \    /         |      .
       \    \/    \/    /  /      \      |    ___    |  /    ______|  .
@@ -45,25 +45,28 @@ func main() {
                             .                                        .
   .                           .         .               .                 .
              .                                   .            .
-________________________________________________________________________
-|:..                                                      :::::%%%%%%HH|
-|%%%:::::..        S t a r s h i p s  &  P i l o t s         ::::::%%%%|
-|HH%%%%%:::::....._______________________________________________::::::|`
+___________________________________________________________________________
+|:..                                                         :::::%%%%%%HH|
+|%%%:::::..          S t a r s h i p s  &  P i l o t s          ::::::%%%%|
+|HH%%%%%:::::.....__________________________________________________::::::|`
 
 	fmt.Printf("%v\n\n", s)
 
-	url := "https://swapi.dev/api/starships/?page=1"
-	starships := get(url)
+	for j := 1; j < 5; j++ {
+		url := fmt.Sprintf("https://swapi.dev/api/starships/?page=%d", j)
+		starships := get(url)
 
-	for i := 0; i < len(starships.Ships); i++ {
-		if len(starships.Ships[i].Pilots) == 0 {
-			i++
-		} else {
-			fmt.Println(starships.Ships[i].Name)
+		for i := 0; i < len(starships.Ships); i++ {
+			if len(starships.Ships[i].Pilots) == 0 {
+				i++
+			} else {
+				fmt.Println(starships.Ships[i].Name)
 
-			getPilots(starships, i)
+				getPilots(starships, i)
+			}
 		}
 	}
+
 }
 
 func get(url string) Response {
@@ -87,7 +90,7 @@ func get(url string) Response {
 
 func getPilots(starships Response, i int) {
 	p := starships.Ships[i].Pilots
-	fmt.Println("Pilots: ")
+	fmt.Println(" Pilots: ")
 	for _, url := range p {
 		pilot := get(url)
 		trimmedPilot := strings.Trim(pilot.Name, "[]{}")
