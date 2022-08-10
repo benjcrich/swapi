d7b40f6d4006b5358ae3e0d1ed5cfa0f1196644a
Benj Rich <benjrich@divvy.co>
2022-08-10 12:22:16 -0600

    modularize

--- benjcrich/main.go
+++ benjcrich/main.go
@@ -1,28 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/peterhellberg/swapi"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	// "reflect"
 )
 
-func main() {
-	c := swapi.DefaultClient
-
-	if ship, err := c.Starship(10); err == nil {
-		fmt.Println("Name: ", ship.Name)
-		p := ship.PilotURLs
-		pilots := getPilotName(p)
-	}
-
-	// for i := 1; i < 20; i++ {
-	// 	if ship, err := c.Starship(10); err == nil {
-	// 		fmt.Println("Name: ", ship.Name)
-	// 		fmt.Println("Pilots: ", ship.PilotURLs)
-	// 	}
-	// }
+type Response struct {
+	Name  string             `json:"name"`
+	Ships []StarshipResponse `json:"results"`
 }
 
-func getPilotName(p string) (string) {
-	
+type StarshipResponse struct {
+	Name   string   `json:"name"`
+	Pilots []string `json:"pilots"`
+}
+
+func main() {
+	url := "https://swapi.dev/api/starships/?page=2"
+	starships := get(url)
+
+	for i := 0; i < len(starships.Ships); i++ {
+		if len(starships.Ships[i].Pilots) == 0 {
+			i++
+		} else {
+			fmt.Println(starships.Ships[i].Name)
+
+			getPilots(starships, i)
+		}
+	}
+}
+
+func get(url string) Response {
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var starships Response
+	json.Unmarshal(responseData, &starships)
+
+	return starships
+}
+
+func getPilots(starships Response, i int) {
+	p := starships.Ships[i].Pilots
+
+	for _, url := range p {
+		pilot := get(url)
+		trimPilot := strings.Trim(pilot, "[]{}")
+		fmt.Printf("    %s\n", trimPilot)
+	}
 }
